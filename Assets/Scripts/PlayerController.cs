07bf4a5488193dde4afeba961ee65c58f61e5677
Zachary Kidd-Smith <34440763+FlyingFLobster@users.noreply.github.com>
2022-01-29 11:34:22 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+//@author Zachary Kidd-Smith
+// Script for the main player character (Hazzah!), should handle all player controlled input during the gameplay (Eg: Walking around, interaction buttons, etc).
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int playerSpeed = 0;
+
+    private Rigidbody rb;
+    private Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = 0;
+        float verticalInput = 0;
+
+        // Check inputs.
+        if (Input.GetButton("Horizontal")) // Negative = left, Positive = right
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                horizontalInput += playerSpeed;
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                horizontalInput -= playerSpeed;
+            }
+        }
+        // Check inputs.
+        if (Input.GetButton("Vertical")) // Negative = down, Positive = up
+        {
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                verticalInput += playerSpeed;
+            }
+            else if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                verticalInput -= playerSpeed;
+            }
+        }
+
+        tr.position = tr.position + new Vector3(horizontalInput * Time.deltaTime, 0.0f, verticalInput * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        // Apply change in movement, currently uses the old unity input system, going to try to avoid physics.
+
+    }
+}
